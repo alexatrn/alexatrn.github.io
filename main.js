141a979233f80dcdd40af7fb709fe0db86ddb2a9
2alin <alinattus@gmail.com>
2018-04-23 22:30:07 -0500

    Add wheen scroll functionality; Add Next buttons functionality

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ var main = document.querySelector('main');
 var sections = document.querySelectorAll('section');
 var posSections = [];
 var goTop = document.querySelector('#go-top');
-var currentIndexSection; //values: 0,1,2...
-var currentTopPosition;
+var currentSIndex; //values: 0,1,2...
+var isScrolling = false;
+var nextBtns = document.querySelectorAll('span.next')
 
 
 
@@ -27,8 +28,8 @@ fillPositions();
 
 //resize events
 window.addEventListener('resize', function() {
-  fillPositions();
-  // calcCurrentPosValues();
+  fillPositions();  // update positions because of resizing
+  goCurrentSection(); // always endup showing a whole section
 });
 
 
@@ -57,38 +58,103 @@ goTop.addEventListener('click', function() {
   scrollToPos(0);
 });
 
-// sections[0].textContent = "hello";
+
+//move to the next/previous section when mouse wheel is scrolled
+window.addEventListener('wheel', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  console.log('wheel scrolled');
+  if (isScrolling){
+    // avoid executing multiple times on one wheel action
+    return;
+  }
+
+  isScrolling = true;
+
+  if (e.deltaY > 0){
+    if(window.scrollY < posSections[currentSIndex]){
+      //adjust the section if it's slightly under top window border
+      goCurrentSection()
+    } else {
+      goNextSection();
+    }
+    isScrolling = false; //wheel scroll OFF
+  } else if(e.deltaY < 0){
+    if(window.scrollY > posSections[currentSIndex]){
+      //adjusts section if it's slightly over the top window border
+      goCurrentSection();
+    } else {
+      goPreviousSection();
+    }
+    isScrolling = false; //wheel scroll OFF
+  }
+
+});
+
 
 //add action when scrolling 
-// window.addEventListener('scroll', () => {
-//   let direction = 0;
-//   if ((window.scrollY - currentTopPosition) > 0) {
-//     direction = 1;
-//   } else if ((window.scrollY - currentTopPosition) < 0) {
-//     direction = -1;
-//   }
-//   console.log("scroll direction: ", direction);
-//   calcCurrentPosValues();
-// });
-// // place section in place after scroll
-// function sectionScrolled(direction, section){
-//   if (direction > 0) {
-//     if(window.scrollY) 
-//   }
-// }
+window.addEventListener('scroll', function() {
+ 
+  setCurrentSection();
+});
 
 
 // // values to calculate when document loads
-// function calcCurrentPosValues(){
-//   currentTopPosition = window.scrollY;
-//   for (let k = 0; k < sections.length; k++){
-//     if (currentTopPosition >= (posSections[k] - 0.25*window.innerHeight)){
-//       //added 0.25 for nav button highlight
-//       currentIndexSection = k;
-//     } else {
-//       break;
-//     }
-//   }
-//   console.log("current position values calculated: ", currentTopPosition, currentIndexSection);
-// }
-// calcCurrentPosValues();
+function setCurrentSection(){
+  for (var k = 0; k < sections.length; k++){
+    if (window.scrollY >= (posSections[k] - 0.2*window.innerHeight)){
+      //added 0.25 for nav button highlight
+      currentSIndex = k;
+    } else {
+      break;
+    }
+  }
+  console.log("current position values calculated: ", currentSIndex);
+}
+setCurrentSection();
+
+
+//move to next section
+function goNextSection(){
+  var nextSIndex;
+  if( currentSIndex === sections.length){
+    //last section, stays or adjusts
+    nextSIndex = currentSIndex;
+  } else {
+    //increase section index by one
+    nextSIndex = currentSIndex + 1;
+  }
+  //move to position of next section
+  scrollToPos(posSections[nextSIndex]);
+}
+
+//move to previous section
+function goPreviousSection(){
+  var previousSIndex;
+  if( currentSIndex === 0){
+    //first section, stays or adjusts
+    previousSIndex = currentSIndex;
+  } else {
+    //decrease section index by one
+    previousSIndex = currentSIndex - 1;
+  }
+  //move to position of previous section
+  scrollToPos(posSections[previousSIndex]);
+}
+
+//adjust position of current section (to be displayed fully)
+function goCurrentSection(){
+  scrollToPos(posSections[currentSIndex]);
+}
+
+//add functionality to every 'next' button
+function linkNextBtns(){
+  for(var k = 0; k < sections.length-1; k++){
+    nextBtns[k].addEventListener('click', function(e) {
+      scrollToPos(posSections[
+        Number(e.target.parentNode.id.slice(-1))
+      ]);
+    });
+  }
+}
+linkNextBtns();
